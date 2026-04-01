8590b6641100cbccc5e8276c3e6b04f3bc20896b
Steven Carpenter <steven@sjcarpenter.com>
2026-03-31 22:54:11 -0600

    feat: update Brewfile and configuration for new tools and settings

---
+++ dot_config/zsh/profile.d/personal-shell-functions.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+# Load Hippo CLI functions
+source /Users/carpenter/projects/hippo/shell/hippo-env.zsh
+source /Users/carpenter/projects/hippo/shell/hippo.zsh
+
+# Vite Plus CLI functions
+source "$HOME/.vite-plus/env"
+
+# ghcup (Haskell toolchain manager) why am I even doing this in 2026
+#[ -f "/Users/carpenter/.ghcup/env" ] && . "/Users/carpenter/.ghcup/env" # ghcup-env
+
+# Mill build tool completions
+#source ~/.local/share/mill/completion/mill-completion.sh # MILL_SOURCE_COMPLETION_LINE
