fd5a239728ef3a5cc7541b95d712f38b8553b26b
Steven Carpenter <steven@sjcarpenter.com>
2026-04-25 00:14:25 -0600

    fix: update paths for Hippo CLI and ghcup in personal shell functions

--- dot_config/zsh/profile.d/personal-shell-functions.zsh
+++ dot_config/zsh/profile.d/personal-shell-functions.zsh
@@ -4,14 +4,14 @@
 alias burp='brew update && brew upgrade && vp update -g @github/copilot && claude update && opencode upgrade'
 
 # Load Hippo CLI functions
-source /Users/carpenter/.local/share/shell/hippo-env.zsh
-source /Users/carpenter/.local/share/shell/hippo.zsh
+source "$HOME/.local/share/hippo-brain/shell/hippo-env.zsh"
+source "$HOME/.local/share/hippo-brain/hippo.zsh"
 
 # Vite Plus CLI functions
 source "$HOME/.vite-plus/env"
 
 # ghcup (Haskell toolchain manager) why am I even doing this in 2026
-#[ -f "/Users/carpenter/.ghcup/env" ] && . "/Users/carpenter/.ghcup/env" # ghcup-env
+#[ -f "$HOME/.ghcup/env" ] && . "$HOME/.ghcup/env" # ghcup-env
 
 # Mill build tool completions
 #source ~/.local/share/mill/completion/mill-completion.sh # MILL_SOURCE_COMPLETION_LINE
